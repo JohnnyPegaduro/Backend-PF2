d5c8c75b39741b19fbafd8d860a6727cd1e0035f
JohnnyPegaduro <matiasdeluca2000@gmail.com>
2022-11-01 20:40:31 -0300

    agregando controllers

---
+++ src/controllers/cart.controller.js
@@ -0,0 +1,89 @@
+//! MODELS /////////////////////////////////
+import Container from "../models/Files Pers/Container.js";
+const contenedor = new Container("src/db/products.json");
+
+import Cart from "../models/Files Pers/Cart.js";
+const cart = new Cart("src/db/cart.json");
+//! MODELS /////////////////////////////////
+const controller = {};
+
+controller.newCart = async (req, res) => {
+    const data = await cart.newCart();
+    res.status(200).json({
+        date: `${data.timestamp}`,
+        message: "Se ha creado un nuevo carrito",
+        id: `${data.id}`,
+    });
+};
+
+controller.deleteCart = async (req, res) => {
+    const data = await cart.deleteCartById(req.params.id);
+    data
+        ?
+        res.status(200).json({
+            message: `Se ha eliminado el carrito`,
+            "cart deleted": `${req.params.id}`,
+        }) :
+        res
+        .status(404)
+        .json({
+            message: "No se ha encontrado el carrito. No existe"
+        });
+};
+
+controller.getProductsInCart = async (req, res) => {
+    const data = await cart.getCartById(req.params.id);
+    if (data === null) {
+        res
+            .status(200)
+            .json({
+                error: "Not found",
+                message: "No se encontró el carrito"
+            });
+    } else if (data.products.length > 0) {
+        res.status(200).json({
+            message: "Se obtuvieron los productos del carrito",
+            "cart id": data.id,
+            products: data.products,
+        });
+    } else {
+        res.status(200).json({
+            message: "Not found",
+            "cart id": data.id,
+            products: "El carrito no tiene productos",
+        });
+    }
+};
+
+controller.saveProductInCart = async (req, res) => {
+    const productToAdd = await contenedor.getById(req.body.id);
+
+    const data = await cart.addProductToCart(req.params.id, productToAdd);
+
+    data != null ?
+        res.status(200).json({
+            message: "Se añadió un producto al carrito",
+            "products in cart": data,
+        }) :
+        res.status(200).json({
+            error: "No se puede añadir el producto",
+            message: "El carrito no existe",
+        });
+};
+
+controller.deleteProductInCart = async (req, res) => {
+    const {
+        id,
+        id_prod
+    } = req.params;
+    const data = await cart.deleteProductInCartById(id, id_prod);
+    data != undefined ?
+        res.status(200).json({
+            message: `Se ha eliminado el producto ${data.title} del carrito ${id}`,
+        }) :
+        res.status(200).json({
+            error: "No existe el producto en el carrito"
+        });
+};
+
+export default controller;
---
+++ src/controllers/products.controllers.js
@@ -0,0 +1,77 @@
+//! CONTENEDOR /////////////////////////////////
+import Container from "../models/Files Pers/Container.js";
+const contenedor = new Container("src/db/products.json");
+//! CONTENEDOR /////////////////////////////////
+const controller = {};
+
+//? DEVUELVE TODOS LOS PRODUCTOS
+
+controller.getAll = async (req, res) => {
+    const data = await contenedor.getAll();
+    res.render("products", {
+        products: data
+    });
+};
+
+//? DEVUELVE UN PRODUCTO SEGÚN SU ID
+
+controller.getById = async (req, res) => {
+    const data = await contenedor.getById(req.params.id);
+
+    //! Si el id generado no coincide con ningún producto, devuelve null; de lo contrario, envía la información solicitada
+    data
+        ?
+        res.status(200).json(data) :
+        res.status(404).json({
+            error: "Producto no encontrado"
+        });
+};
+
+//? RECIBE Y AGREGA UN PRODUCTO, Y LO DEVUELVE CON SU ID ASIGNADO
+
+controller.post = async (req, res) => {
+    const data = await contenedor.save(req.body);
+    data == null ?
+        res
+        .status(500)
+        .json({
+            message: `[[${req.body.title}]] ya existe en el archivo`
+        }) :
+        res.status(200).render("index");
+};
+
+//? RECIBE Y ACTUALIZA UN PRODUCTO SEGÚN SU ID
+
+controller.put = async (req, res) => {
+    const {
+        id
+    } = req.params;
+    const newObject = req.body;
+    const data = await contenedor.update(+id, newObject);
+
+    data != null ?
+        res.status(200).json({
+            message: `Producto ${id} modificado con éxito`,
+            "new product": newObject,
+        }) :
+        res.status(404).json({
+            error: "Producto no encontrado"
+        });
+};
+
+//? ELIMINA UN PRODUCTO SEGÚN SU ID
+
+controller.delete = async (req, res) => {
+    const data = await contenedor.deleteById(req.params.id);
+    data
+        ?
+        res.status(200).send({
+            message: `Se ha eliminado el producto`,
+            "product deleted": data,
+        }) :
+        res.status(404).send({
+            message: "No se ha encontrado el producto"
+        });
+};
+
+export default controller;
